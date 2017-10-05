af578bd24469bcc660f0a90da457cfd47390259c
Moch. Lutfi <kapten.lufi@gmail.com>
2017-10-05 12:40:53 +0700

    Added search word 2 problem

--- backtrack/word_search.go
+++ backtrack/word_search.go
@@ -1,8 +1,9 @@
 package backtrack
 
 import (
-	"fmt"
+	"sort"
 )
+
 // WordSearch is solution for word search problem
 // Given a 2D board and a word, find if the word exists in the grid.
 // 
@@ -31,14 +32,13 @@ func(w *WordSearch) Exist(word string) bool {
 				return true;
 			}
 		}
-		fmt.Println()
 	}
 	return false
 }
 
 // dfs deep first search
 func(w *WordSearch) dfs(board [][]byte, word string, i, j int) bool {
-	fmt.Println(board)
+	//fmt.Println(board)
 	if len(word)==0{
 		return true
 	}
@@ -50,7 +50,7 @@ func(w *WordSearch) dfs(board [][]byte, word string, i, j int) bool {
 		board[i][j]!=word[0]{
 			return false
 		}
-	fmt.Println(board, word, i, j, len(board),len(board[0]), board[i][j]!=word[0], word[0], word[1:], board[i][j]  )
+	//fmt.Println(board, word, i, j, len(board),len(board[0]), board[i][j]!=word[0], word[0], word[1:], board[i][j]  )
 	tmp := board[i][j]
 	board[i][j] = '#'
 	result := w.dfs(board, word[1:], i+1, j) ||
@@ -66,4 +66,52 @@ func NewWordSearch(board [][]byte) *WordSearch{
 	return &WordSearch{
 		board:board,
 	}
+}
+
+// findWords to find words from boards
+// Given a 2D board and a list of words from the dictionary, find all words in the board.
+// Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+// For example,
+// Given words = ["oath","pea","eat","rain"] and board =
+// [
+//   ['o','a','a','n'],
+//   ['e','t','a','e'],
+//   ['i','h','k','r'],
+//   ['i','f','l','v']
+// ]
+// Return ["eat","oath"].
+// Note:
+// You may assume that all inputs are consist of lowercase letters a-z.
+// You would need to optimize your backtracking to pass the larger test. Could you stop backtracking earlier?
+// If the current candidate does not exist in all words' prefix, you could stop backtracking immediately. What kind of data structure could answer such query efficiently? Does a hash table work? Why or why not? How about a Trie? If you would like to learn how to implement a basic trie, please work on this problem: Implement Trie (Prefix Tree) first.
+func (w *WordSearch)FindWords(words []string) []string {
+	words = removeDuplicates(words)
+	sort.Strings(words)
+	var results = []string{};
+	for _, word := range words{
+		if w.Exist(word){
+			results = append(results, word)
+		}
+	}
+
+	return results
+}
+
+func removeDuplicates(elements []string) []string {
+	// Use map to record duplicates as we find them.
+	encountered := map[string]bool{}
+	result := []string{}
+
+	for v := range elements {
+			if encountered[elements[v]] == true {
+					// Do not add duplicate.
+			} else {
+					// Record this element as an encountered element.
+					encountered[elements[v]] = true
+					// Append to result slice.
+					result = append(result, elements[v])
+			}
+	}
+	// Return the new slice.
+	return result
 }
--- backtrack/word_search_test.go
+++ backtrack/word_search_test.go
@@ -18,3 +18,28 @@ func TestCaseA(t *testing.T) {
 	
 	assert.Equal(nws.Exist("AAB"), true, "should return true")
 }
+
+func TestCaseB(t *testing.T) {
+	assert := assert.New(t)
+	board := [][]byte{
+		[]byte("oaan"),
+		[]byte("etae"),
+		[]byte("ihkr"),
+		[]byte("ihkr"),
+	}
+
+	nws := backtrack.NewWordSearch(board)
+	words := []string{"oath","pea","eat","rain"}
+	assert.Equal(nws.FindWords(words), []string{"eat","oath"}, "should return []string{\"oath\",\"eat\"}")
+}
+
+func TestCaseC(t *testing.T) {
+	assert := assert.New(t)
+	board := [][]byte{
+		[]byte("a"),
+	}
+
+	nws := backtrack.NewWordSearch(board)
+	words := []string{"a","a"}
+	assert.Equal(nws.FindWords(words), []string{"a"}, "should return a")
+}
